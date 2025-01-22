b3871b5926c0c2cd98760f1678716880a4b25176
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-01-23 05:08:41 +0800

    homework exercise1

---
+++ Advanced JS/Class_03 Homework/homework_01/ajaxApiCities.js
@@ -0,0 +1,56 @@
+// # Homework 1
+// Create a button
+// When the button is clicked, get the data from the  api with an AJAX call.
+// Print the details for the first 10 cities in an **unordered list**. If you are experiencing CORS problem, try using
+// ```javascript
+// mode: "no-cors",
+// headers: {
+//         "Access-Control-Allow-Origin": "*",
+//       }
+// ```
+// in the ajax call.
+// If you are still having problem, use https://pokeapi.co/api/v2/pokemon and print details for the first ten pokemons
+// of the result.
+// * Use JQuery AJAX call
+
+$(document).ready(function () {
+  $("#fetchDataBtn").click(function () {
+    $.ajax({
+      url: "https://api.openaq.org/v1/cities",
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      success: function (response) {
+        console.log("OpenAQ API response:", response);
+        if (response.cities) {
+          let cities = response.cities.slice(0, 10);
+          for (let city of cities) {
+            $("#dataList").append(`<li>${city}</li>`);
+          }
+        } else {
+          console.log("No cities found in the response");
+        }
+      },
+      error: function () {
+        console.log("OpenAQ API call failed, fetching Pokémon data");
+        $.ajax({
+          url: "https://pokeapi.co/api/v2/pokemon",
+          // url: 'https://pokeapi.co/api/v2/pokemon?limit=10',
+          success: function (response) {
+            let pokemons = response.results.slice(0, 10);
+            // let pokemons = response.results;
+            console.log("Pokémon API response:", pokemons);
+            for (let pokemon of pokemons) {
+              $("#dataList").append(`<li>${pokemon.name}</li>`);
+            }
+          },
+          error: function (error) {
+            console.log("An error occurred fetching Pokémon data");
+            console.log(error);
+          },
+        });
+      },
+    });
+  });
+});
